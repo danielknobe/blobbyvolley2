9dc524a85ba6951ca30f0ba3e122aba6e96f25f4
yonibear <yonibear@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2006-08-26 19:33:49 +0000

    Cleaned up build system and included lua (first try)

--- trunk/src/ScriptedInputSource.cpp
+++ trunk/src/ScriptedInputSource.cpp
@@ -3,46 +3,9 @@
 
 extern "C"
 {
-#if defined(WIN32)
-	#include "lua.h"
-	#include "lauxlib.h"
-	#include "lualib.h"
-#else
-#if defined(HAVE_LUA51_LUA_H)
-	#include <lua5.1/lua.h>
-#elif defined(HAVE_LUA50_LUA_H)
-	#include <lua50/lua.h>
-#elif defined(HAVE_LUA_LUA_H)
-	#include <lua/lua.h>
-#elif defined(HAVE_LUA_H)
-	#include <lua.h>
-#else
-	#error Missing lua.h
-#endif
-
-#if defined(HAVE_LUA51_LAUXLIB_H)
-	#include <lua5.1/lauxlib.h>
-#elif defined(HAVE_LUA50_LAUXLIB_H)
-	#include <lua50/lauxlib.h>
-#elif defined(HAVE_LUA_LAUXLIB_H)
-	#include <lua/lauxlib.h>
-#elif defined(HAVE_LAUXLIB_H)
-	#include <lauxlib.h>
-#else
-	#error Missing lauxlib.h
-#endif
-#if defined(HAVE_LUA51_LUALIB_H)
-	#include <lua5.1/lualib.h>
-#elif defined(HAVE_LUA50_LUALIB_H)
-	#include <lua50/lualib.h>
-#elif defined(HAVE_LUA_LUALIB_H)
-	#include <lua/lualib.h>
-#elif defined(HAVE_LUALIB_H)
-	#include <lualib.h>
-#else
-	#error Missing lualib.h
-#endif
-#endif
+#include "lua/lua.h"
+#include "lua/lauxlib.h"
+#include "lua/lualib.h"
 }
 
 #include <physfs.h>
