0c558672adc1c4fe50ffb433a14cc4d56290b8f3
danielknobe <54905085+danielknobe@users.noreply.github.com>
2006-09-13 18:45:37 +0000

    - Better experimental gamepadsupport :) (now for 2 players) - Better Mousesupport - Some Bugfixes - Some Cleanups - kicked old #ifdef which was importent, before gamepad was implemented, atm it's not posible to play with a handheld. In one of the next releases it will be posible :), again

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -11,14 +11,9 @@ InputManager::InputManager()
 	assert (mSingleton == 0);
 	mSingleton = this;
 	mRunning = true;
-	mJoystick = 0;
+	mJoystickLeft = 0;
+	mJoystickRight = 0;
 
-	mNumberOfJoysticks = SDL_NumJoysticks();
-	if (mNumberOfJoysticks)
-	{
-		mJoystick = SDL_JoystickOpen(0);
-	}
-	
 	// initialize the SDLKeys for ingame input
 	configFileToCurrentConfigForLeftKeyboard();
 	configFileToCurrentConfigForRightKeyboard();
@@ -28,7 +23,15 @@ InputManager::InputManager()
 	configFileToCurrentConfigForLeftDevice();
 	configFileToCurrentConfigForRightDevice();
 
-	mIngameDevicesAvailable = false;
+	mNumberOfJoysticks = SDL_NumJoysticks();
+	if (mNumberOfJoysticks)
+	{
+		mJoystickLeft = SDL_JoystickOpen(mLeftBlobbyJoystickNumber);
+		mJoystickRight = SDL_JoystickOpen(mRightBlobbyJoystickNumber);
+	}
+
+	mLeftDeviceAvailable = false;
+	mRightDeviceAvailable = false;
 }
 
 InputManager::~InputManager()
@@ -41,9 +44,10 @@ InputManager::~InputManager()
 	currentConfigToConfigFileForRightDevice();
 }
 
-bool InputManager::createIngameInputDevices()
+bool InputManager::beginGame(int side)
 {
-	if (mIngameDevicesAvailable == false)
+
+	if (side == LEFT_PLAYER && mLeftDeviceAvailable == false)
 	{
 		if (mLeftBlobbyInputDevice == MOUSE)
 			mInputDevice[LEFT_PLAYER] = new MouseInputDevice(LEFT_PLAYER);
@@ -54,7 +58,7 @@ bool InputManager::createIngameInputDevices()
 			mLeftBlobbyKeyboardJump);
 
 		if (mLeftBlobbyInputDevice == JOYSTICK)
-			mInputDevice[LEFT_PLAYER] = new JoystickInputDevice(mJoystick,
+			mInputDevice[LEFT_PLAYER] = new JoystickInputDevice(mJoystickLeft,
 			mLeftBlobbyLeftJoystickMove,
 			mLeftBlobbyRightJoystickMove,
 			mLeftBlobbyJoystickJump,
@@ -62,6 +66,11 @@ bool InputManager::createIngameInputDevices()
 			mLeftBlobbyJoystickLeftJump,
 			mLeftBlobbyJoystickRightJump);
 
+		mLeftDeviceAvailable = true;
+	}
+
+	if (side == RIGHT_PLAYER && mRightDeviceAvailable == false)
+	{
 		if (mRightBlobbyInputDevice == MOUSE)
 			mInputDevice[RIGHT_PLAYER] = new MouseInputDevice(RIGHT_PLAYER);	
 
@@ -71,25 +80,32 @@ bool InputManager::createIngameInputDevices()
 			mRightBlobbyKeyboardJump);
 
 		if (mRightBlobbyInputDevice == JOYSTICK)
-			mInputDevice[RIGHT_PLAYER] = new JoystickInputDevice(0,
+			mInputDevice[RIGHT_PLAYER] = new JoystickInputDevice(mJoystickRight,
 			mRightBlobbyLeftJoystickMove,
 			mRightBlobbyRightJoystickMove,
 			mRightBlobbyJoystickJump,
 			mRightBlobbyJoystickDiagonal,
 			mRightBlobbyJoystickLeftJump,
 			mRightBlobbyJoystickRightJump);
+
+		mRightDeviceAvailable = true;
 	}
+
 }
 
-bool InputManager::deleteIngameInputDevices()
+bool InputManager::endGame()
 {
-if (mIngameDevicesAvailable == true)
+if (mLeftDeviceAvailable == true)
 	{
 		delete mInputDevice[LEFT_PLAYER];
 		mInputDevice[LEFT_PLAYER] = NULL;
-
+		mLeftDeviceAvailable = false;
+	}
+if (mRightDeviceAvailable == true)
+	{
 		delete mInputDevice[RIGHT_PLAYER];
 		mInputDevice[RIGHT_PLAYER] = NULL;
+		mRightDeviceAvailable = false;
 	}
 }
 
@@ -127,10 +143,9 @@ void InputManager::updateInput()
 	SDL_Event event;
 
 
-	if (mJoystick)
+	if (mJoystickLeft || mJoystickRight)
 		SDL_JoystickUpdate();
 
-	// AUSLAUFMODELLAUFBAU==================================================
 	while (SDL_PollEvent(&event))
 	switch (event.type)
 	{
@@ -189,36 +204,13 @@ void InputManager::updateInput()
 #endif
 	}
 
-#if defined(__arm__) && defined(linux)
-// kicked in next rev. by DanielKnobe (DON'T FORGET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
-	mInput[0] = PlayerInput(
-		SDL_JoystickGetButton(mJoystick, 2) ||
-			SDL_JoystickGetButton(mJoystick, 1),
-		SDL_JoystickGetButton(mJoystick, 6) ||
-			SDL_JoystickGetButton(mJoystick, 7),
-		SDL_JoystickGetButton(mJoystick, 0) ||
-			SDL_JoystickGetButton(mJoystick, 1) ||
-			SDL_JoystickGetButton(mJoystick, 7));
-	mInput[1] = PlayerInput(
-		SDL_JoystickGetButton(mJoystick, 12),
-		SDL_JoystickGetButton(mJoystick, 13),
-		SDL_JoystickGetButton(mJoystick, 14)
-	);
-	if (
-		SDL_JoystickGetButton(mJoystick, 10) &&
-		SDL_JoystickGetButton(mJoystick, 11)
-	)
-		mRunning = false;
-#else
-
 	// Device gives status to the playerinput
-	if (mIngameDevicesAvailable == true)
-	{
+	if (mLeftDeviceAvailable == true)
 		mInputDevice[0]->transferInput(mInput[0]);
+	if (mRightDeviceAvailable == true)
 		mInputDevice[1]->transferInput(mInput[1]);
-	}
-#endif
 }
+
 
 // GUI-Methods
 bool InputManager::up()
@@ -269,257 +261,257 @@ bool InputManager::running()
 
 // declaration of the keymap
 InputKeyMap InputManager::mKeyMap[] = {
-	"",SDLK_UNKNOWN,
-	"",SDLK_FIRST,
-	"backspace",SDLK_BACKSPACE,
-	"tabulator",SDLK_TAB,
-	"",SDLK_CLEAR,
-	"return",SDLK_RETURN,
-	"pause",SDLK_PAUSE,
-	"",SDLK_ESCAPE,
-	"space",SDLK_SPACE,
-	"exclaim",SDLK_EXCLAIM,
-	"quotedouble",SDLK_QUOTEDBL,
-	"#",SDLK_HASH,
-	"$",SDLK_DOLLAR,
-	"&",SDLK_AMPERSAND,
-	"'",SDLK_QUOTE,
-	"(",SDLK_LEFTPAREN,
-	")",SDLK_RIGHTPAREN,
-	"*",SDLK_ASTERISK,
-	"+",SDLK_PLUS,
-	",",SDLK_COMMA,
-	"-",SDLK_MINUS,
-	".",SDLK_PERIOD,
-	"/",SDLK_SLASH,
-	"0",SDLK_0,
-	"1",SDLK_1,
-	"2",SDLK_2,
-	"3",SDLK_3,
-	"4",SDLK_4,
-	"5",SDLK_5,
-	"6",SDLK_6,
-	"7",SDLK_7,
-	"8",SDLK_8,
-	"9",SDLK_9,
-	":",SDLK_COLON,
-	";",SDLK_SEMICOLON,
-	"<",SDLK_LESS,
-	"=",SDLK_EQUALS,
-	">",SDLK_GREATER,
-	"?",SDLK_QUESTION,
-	"@",SDLK_AT,
+	{ "",SDLK_UNKNOWN },
+	{ "",SDLK_FIRST },
+	{ "backspace",SDLK_BACKSPACE },
+	{ "tabulator",SDLK_TAB },
+	{ "",SDLK_CLEAR },
+	{ "return",SDLK_RETURN },
+	{ "pause",SDLK_PAUSE },
+	{ "",SDLK_ESCAPE },
+	{ "space",SDLK_SPACE },
+	{ "exclaim",SDLK_EXCLAIM },
+	{ "quotedouble",SDLK_QUOTEDBL },
+	{ "#",SDLK_HASH },
+	{ "$",SDLK_DOLLAR },
+	{ "&",SDLK_AMPERSAND },
+	{ "'",SDLK_QUOTE },
+	{ "(",SDLK_LEFTPAREN },
+	{ ")",SDLK_RIGHTPAREN },
+	{ "*",SDLK_ASTERISK },
+	{ "+",SDLK_PLUS },
+	{ ",",SDLK_COMMA },
+	{ "-",SDLK_MINUS },
+	{ ".",SDLK_PERIOD },
+	{ "/",SDLK_SLASH },
+	{ "0",SDLK_0 },
+	{ "1",SDLK_1 },
+	{ "2",SDLK_2 },
+	{ "3",SDLK_3 },
+	{ "4",SDLK_4 },
+	{ "5",SDLK_5 },
+	{ "6",SDLK_6 },
+	{ "7",SDLK_7 },
+	{ "8",SDLK_8 },
+	{ "9",SDLK_9 },
+	{ ":",SDLK_COLON },
+	{ ";",SDLK_SEMICOLON },
+	{ "<",SDLK_LESS },
+	{ "=",SDLK_EQUALS },
+	{ ">",SDLK_GREATER },
+	{ "?",SDLK_QUESTION },
+	{ "@",SDLK_AT },
 
 	/* 
 	   Skip uppercase letters
 	 */
-	"[",SDLK_LEFTBRACKET,
-	"backslash",SDLK_BACKSLASH,
-	"]",SDLK_RIGHTBRACKET,
-	"^",SDLK_CARET,
-	"_",SDLK_UNDERSCORE,
-	"`",SDLK_BACKQUOTE,
-	"a",SDLK_a,
-	"b",SDLK_b,
-	"c",SDLK_c,
-	"d",SDLK_d,
-	"e",SDLK_e,
-	"f",SDLK_f,
-	"g",SDLK_g,
-	"h",SDLK_h,
-	"i",SDLK_i,
-	"j",SDLK_j,
-	"k",SDLK_k,
-	"l",SDLK_l,
-	"m",SDLK_m,
-	"n",SDLK_n,
-	"o",SDLK_o,
-	"p",SDLK_p,
-	"q",SDLK_q,
-	"r",SDLK_r,
-	"s",SDLK_s,
-	"t",SDLK_t,
-	"u",SDLK_u,
-	"v",SDLK_v,
-	"w",SDLK_w,
-	"x",SDLK_x,
-	"y",SDLK_y,
-	"z",SDLK_z,
-	"del",SDLK_DELETE,
+	{ "[",SDLK_LEFTBRACKET },
+	{ "backslash",SDLK_BACKSLASH },
+	{ "]",SDLK_RIGHTBRACKET },
+	{ "^",SDLK_CARET },
+	{ "_",SDLK_UNDERSCORE },
+	{ "`",SDLK_BACKQUOTE },
+	{ "a",SDLK_a },
+	{ "b",SDLK_b },
+	{ "c",SDLK_c },
+	{ "d",SDLK_d },
+	{ "e",SDLK_e },
+	{ "f",SDLK_f },
+	{ "g",SDLK_g },
+	{ "h",SDLK_h },
+	{ "i",SDLK_i },
+	{ "j",SDLK_j },
+	{ "k",SDLK_k },
+	{ "l",SDLK_l },
+	{ "m",SDLK_m },
+	{ "n",SDLK_n },
+	{ "o",SDLK_o },
+	{ "p",SDLK_p },
+	{ "q",SDLK_q },
+	{ "r",SDLK_r },
+	{ "s",SDLK_s },
+	{ "t",SDLK_t },
+	{ "u",SDLK_u },
+	{ "v",SDLK_v },
+	{ "w",SDLK_w },
+	{ "x",SDLK_x },
+	{ "y",SDLK_y },
+	{ "z",SDLK_z },
+	{ "del",SDLK_DELETE },
 	/* End of ASCII mapped keysyms */
 
 	/* International keyboard syms */
-	"world0",SDLK_WORLD_0,
-	"world1",SDLK_WORLD_1,
-	"world2",SDLK_WORLD_2,
-	"world3",SDLK_WORLD_3,
-	"world4",SDLK_WORLD_4,
-	"world5",SDLK_WORLD_5,
-	"world6",SDLK_WORLD_6,
-	"world7",SDLK_WORLD_7,
-	"world8",SDLK_WORLD_8,
-	"world9",SDLK_WORLD_9,
-	"world10",SDLK_WORLD_10,
-	"world11",SDLK_WORLD_11,
-	"world12",SDLK_WORLD_12,
-	"world13",SDLK_WORLD_13,
-	"world14",SDLK_WORLD_14,
-	"world15",SDLK_WORLD_15,
-	"world16",SDLK_WORLD_16,
-	"world17",SDLK_WORLD_17,
-	"world18",SDLK_WORLD_18,
-	"world19",SDLK_WORLD_19,
-	"world20",SDLK_WORLD_20,
-	"world21",SDLK_WORLD_21,
-	"world22",SDLK_WORLD_22,
-	"world23",SDLK_WORLD_23,
-	"world24",SDLK_WORLD_24,
-	"world25",SDLK_WORLD_25,
-	"world26",SDLK_WORLD_26,
-	"world27",SDLK_WORLD_27,
-	"world28",SDLK_WORLD_28,
-	"world29",SDLK_WORLD_29,
-	"world30",SDLK_WORLD_30,
-	"world31",SDLK_WORLD_31,
-	"world32",SDLK_WORLD_32,
-	"world33",SDLK_WORLD_33,
-	"world34",SDLK_WORLD_34,
-	"world35",SDLK_WORLD_35,
-	"world36",SDLK_WORLD_36,
-	"world37",SDLK_WORLD_37,
-	"world38",SDLK_WORLD_38,
-	"world39",SDLK_WORLD_39,
-	"world40",SDLK_WORLD_40,
-	"world41",SDLK_WORLD_41,
-	"world42",SDLK_WORLD_42,
-	"world43",SDLK_WORLD_43,
-	"world44",SDLK_WORLD_44,
-	"world45",SDLK_WORLD_45,
-	"world46",SDLK_WORLD_46,
-	"world47",SDLK_WORLD_47,
-	"world48",SDLK_WORLD_48,
-	"world49",SDLK_WORLD_49,
-	"world50",SDLK_WORLD_50,
-	"world51",SDLK_WORLD_51,
-	"world52",SDLK_WORLD_52,
-	"world53",SDLK_WORLD_53,
-	"world54",SDLK_WORLD_54,
-	"world55",SDLK_WORLD_55,
-	"world56",SDLK_WORLD_56,
-	"world57",SDLK_WORLD_57,
-	"world58",SDLK_WORLD_58,
-	"world69",SDLK_WORLD_59,
-	"world60",SDLK_WORLD_60,
-	"world61",SDLK_WORLD_61,
-	"world62",SDLK_WORLD_62,
-	"world63",SDLK_WORLD_63,
-	"world64",SDLK_WORLD_64,
-	"world65",SDLK_WORLD_65,
-	"world66",SDLK_WORLD_66,
-	"world67",SDLK_WORLD_67,
-	"world68",SDLK_WORLD_68,
-	"world69",SDLK_WORLD_69,
-	"world70",SDLK_WORLD_70,
-	"world71",SDLK_WORLD_71,
-	"world72",SDLK_WORLD_72,
-	"world73",SDLK_WORLD_73,
-	"world74",SDLK_WORLD_74,
-	"world75",SDLK_WORLD_75,
-	"world76",SDLK_WORLD_76,
-	"world77",SDLK_WORLD_77,
-	"world78",SDLK_WORLD_78,
-	"world79",SDLK_WORLD_79,
-	"world80",SDLK_WORLD_80,
-	"world81",SDLK_WORLD_81,
-	"world82",SDLK_WORLD_82,
-	"world83",SDLK_WORLD_83,
-	"world84",SDLK_WORLD_84,
-	"world85",SDLK_WORLD_85,
-	"world86",SDLK_WORLD_86,
-	"world87",SDLK_WORLD_87,
-	"world88",SDLK_WORLD_88,
-	"world89",SDLK_WORLD_89,
-	"world90",SDLK_WORLD_90,
-	"world91",SDLK_WORLD_91,
-	"world92",SDLK_WORLD_92,
-	"world93",SDLK_WORLD_93,
-	"world94",SDLK_WORLD_94,
-	"world95",SDLK_WORLD_95,
+	{ "world0",SDLK_WORLD_0 },
+	{ "world1",SDLK_WORLD_1 },
+	{ "world2",SDLK_WORLD_2 },
+	{ "world3",SDLK_WORLD_3 },
+	{ "world4",SDLK_WORLD_4 },
+	{ "world5",SDLK_WORLD_5 },
+	{ "world6",SDLK_WORLD_6 },
+	{ "world7",SDLK_WORLD_7 },
+	{ "world8",SDLK_WORLD_8 },
+	{ "world9",SDLK_WORLD_9 },
+	{ "world10",SDLK_WORLD_10 },
+	{ "world11",SDLK_WORLD_11 },
+	{ "world12",SDLK_WORLD_12 },
+	{ "world13",SDLK_WORLD_13 },
+	{ "world14",SDLK_WORLD_14 },
+	{ "world15",SDLK_WORLD_15 },
+	{ "world16",SDLK_WORLD_16 },
+	{ "world17",SDLK_WORLD_17 },
+	{ "world18",SDLK_WORLD_18 },
+	{ "world19",SDLK_WORLD_19 },
+	{ "world20",SDLK_WORLD_20 },
+	{ "world21",SDLK_WORLD_21 },
+	{ "world22",SDLK_WORLD_22 },
+	{ "world23",SDLK_WORLD_23 },
+	{ "world24",SDLK_WORLD_24 },
+	{ "world25",SDLK_WORLD_25 },
+	{ "world26",SDLK_WORLD_26 },
+	{ "world27",SDLK_WORLD_27 },
+	{ "world28",SDLK_WORLD_28 },
+	{ "world29",SDLK_WORLD_29 },
+	{ "world30",SDLK_WORLD_30 },
+	{ "world31",SDLK_WORLD_31 },
+	{ "world32",SDLK_WORLD_32 },
+	{ "world33",SDLK_WORLD_33 },
+	{ "world34",SDLK_WORLD_34 },
+	{ "world35",SDLK_WORLD_35 },
+	{ "world36",SDLK_WORLD_36 },
+	{ "world37",SDLK_WORLD_37 },
+	{ "world38",SDLK_WORLD_38 },
+	{ "world39",SDLK_WORLD_39 },
+	{ "world40",SDLK_WORLD_40 },
+	{ "world41",SDLK_WORLD_41 },
+	{ "world42",SDLK_WORLD_42 },
+	{ "world43",SDLK_WORLD_43 },
+	{ "world44",SDLK_WORLD_44 },
+	{ "world45",SDLK_WORLD_45 },
+	{ "world46",SDLK_WORLD_46 },
+	{ "world47",SDLK_WORLD_47 },
+	{ "world48",SDLK_WORLD_48 },
+	{ "world49",SDLK_WORLD_49 },
+	{ "world50",SDLK_WORLD_50 },
+	{ "world51",SDLK_WORLD_51 },
+	{ "world52",SDLK_WORLD_52 },
+	{ "world53",SDLK_WORLD_53 },
+	{ "world54",SDLK_WORLD_54 },
+	{ "world55",SDLK_WORLD_55 },
+	{ "world56",SDLK_WORLD_56 },
+	{ "world57",SDLK_WORLD_57 },
+	{ "world58",SDLK_WORLD_58 },
+	{ "world69",SDLK_WORLD_59 },
+	{ "world60",SDLK_WORLD_60 },
+	{ "world61",SDLK_WORLD_61 },
+	{ "world62",SDLK_WORLD_62 },
+	{ "world63",SDLK_WORLD_63 },
+	{ "world64",SDLK_WORLD_64 },
+	{ "world65",SDLK_WORLD_65 },
+	{ "world66",SDLK_WORLD_66 },
+	{ "world67",SDLK_WORLD_67 },
+	{ "world68",SDLK_WORLD_68 },
+	{ "world69",SDLK_WORLD_69 },
+	{ "world70",SDLK_WORLD_70 },
+	{ "world71",SDLK_WORLD_71 },
+	{ "world72",SDLK_WORLD_72 },
+	{ "world73",SDLK_WORLD_73 },
+	{ "world74",SDLK_WORLD_74 },
+	{ "world75",SDLK_WORLD_75 },
+	{ "world76",SDLK_WORLD_76 },
+	{ "world77",SDLK_WORLD_77 },
+	{ "world78",SDLK_WORLD_78 },
+	{ "world79",SDLK_WORLD_79 },
+	{ "world80",SDLK_WORLD_80 },
+	{ "world81",SDLK_WORLD_81 },
+	{ "world82",SDLK_WORLD_82 },
+	{ "world83",SDLK_WORLD_83 },
+	{ "world84",SDLK_WORLD_84 },
+	{ "world85",SDLK_WORLD_85 },
+	{ "world86",SDLK_WORLD_86 },
+	{ "world87",SDLK_WORLD_87 },
+	{ "world88",SDLK_WORLD_88 },
+	{ "world89",SDLK_WORLD_89 },
+	{ "world90",SDLK_WORLD_90 },
+	{ "world91",SDLK_WORLD_91 },
+	{ "world92",SDLK_WORLD_92 },
+	{ "world93",SDLK_WORLD_93 },
+	{ "world94",SDLK_WORLD_94 },
+	{ "world95",SDLK_WORLD_95 },
 
 	/* Numeric keypad */
-	"keypad0",SDLK_KP0,
-	"keypad1",SDLK_KP1,
-	"keypad2",SDLK_KP2,
-	"keypad3",SDLK_KP3,
-	"keypad4",SDLK_KP4,
-	"keypad5",SDLK_KP5,
-	"keypad6",SDLK_KP6,
-	"keypad7",SDLK_KP7,
-	"keypad8",SDLK_KP8,
-	"keypad9",SDLK_KP9,
-	"keypad.",SDLK_KP_PERIOD,
-	"keypad/",SDLK_KP_DIVIDE,
-	"keypad*",SDLK_KP_MULTIPLY,
-	"keypad-",SDLK_KP_MINUS,
-	"keypad+",SDLK_KP_PLUS,
-	"keypadenter",SDLK_KP_ENTER,
-	"keypad",SDLK_KP_EQUALS,
+	{ "keypad0",SDLK_KP0 },
+	{ "keypad1",SDLK_KP1 },
+	{ "keypad2",SDLK_KP2 },
+	{ "keypad3",SDLK_KP3 },
+	{ "keypad4",SDLK_KP4 },
+	{ "keypad5",SDLK_KP5 },
+	{ "keypad6",SDLK_KP6 },
+	{ "keypad7",SDLK_KP7 },
+	{ "keypad8",SDLK_KP8 },
+	{ "keypad9",SDLK_KP9 },
+	{ "keypad.",SDLK_KP_PERIOD },
+	{ "keypad/",SDLK_KP_DIVIDE },
+	{ "keypad*",SDLK_KP_MULTIPLY },
+	{ "keypad-",SDLK_KP_MINUS },
+	{ "keypad+",SDLK_KP_PLUS },
+	{ "keypadenter",SDLK_KP_ENTER },
+	{ "keypad",SDLK_KP_EQUALS },
 
 	/* Arrows + Home/End pad */
-	"up",SDLK_UP,
-	"down",SDLK_DOWN,
-	"right",SDLK_RIGHT,
-	"left",SDLK_LEFT,
-	"insert",SDLK_INSERT,
-	"home",SDLK_HOME,
-	"end",SDLK_END,
-	"pageup",SDLK_PAGEUP,
-	"pagedown",SDLK_PAGEDOWN,
+	{ "up",SDLK_UP },
+	{ "down",SDLK_DOWN },
+	{ "right",SDLK_RIGHT },
+	{ "left",SDLK_LEFT },
+	{ "insert",SDLK_INSERT },
+	{ "home",SDLK_HOME },
+	{ "end",SDLK_END },
+	{ "pageup",SDLK_PAGEUP },
+	{ "pagedown",SDLK_PAGEDOWN },
 
 	/* Function keys */
-	"f1",SDLK_F1,
-	"f2",SDLK_F2,
-	"f3",SDLK_F3,
-	"f4",SDLK_F4,
-	"f5",SDLK_F5,
-	"f6",SDLK_F6,
-	"f7",SDLK_F7,
-	"f8",SDLK_F8,
-	"f9",SDLK_F9,
-	"f10",SDLK_F10,
-	"f11",SDLK_F11,
-	"f12",SDLK_F12,
-	"f13",SDLK_F13,
-	"f14",SDLK_F14,
-	"f15",SDLK_F15,
+	{ "f1",SDLK_F1 },
+	{ "f2",SDLK_F2 },
+	{ "f3",SDLK_F3 },
+	{ "f4",SDLK_F4 },
+	{ "f5",SDLK_F5 },
+	{ "f6",SDLK_F6 },
+	{ "f7",SDLK_F7 },
+	{ "f8",SDLK_F8 },
+	{ "f9",SDLK_F9 },
+	{ "f10",SDLK_F10 },
+	{ "f11",SDLK_F11 },
+	{ "f12",SDLK_F12 },
+	{ "f13",SDLK_F13 },
+	{ "f14",SDLK_F14 },
+	{ "f15",SDLK_F15 },
 
 	/* Key state modifier keys */
-	"numlock",SDLK_NUMLOCK,
-	"capslock",SDLK_CAPSLOCK,
-	"scrollock",SDLK_SCROLLOCK,
-	"rightshift",SDLK_RSHIFT,
-	"leftshift",SDLK_LSHIFT,
-	"rightcontrol",SDLK_RCTRL,
-	"leftcontrol",SDLK_LCTRL,
-	"rightalt",SDLK_RALT,
-	"leftalt",SDLK_LALT,
-	"rmeta",SDLK_RMETA,
-	"lmeta",SDLK_LMETA,
-	"leftwin",SDLK_LSUPER,
-	"rightwin",SDLK_RSUPER,
-	"mode",SDLK_MODE,		/* "Alt Gr" key */
-	"compose",SDLK_COMPOSE,	/* Multi-key compose key */
+	{ "numlock",SDLK_NUMLOCK },
+	{ "capslock",SDLK_CAPSLOCK },
+	{ "scrollock",SDLK_SCROLLOCK },
+	{ "rightshift",SDLK_RSHIFT },
+	{ "leftshift",SDLK_LSHIFT },
+	{ "rightcontrol",SDLK_RCTRL },
+	{ "leftcontrol",SDLK_LCTRL },
+	{ "rightalt",SDLK_RALT },
+	{ "leftalt",SDLK_LALT },
+	{ "rmeta",SDLK_RMETA },
+	{ "lmeta",SDLK_LMETA },
+	{ "leftwin",SDLK_LSUPER },
+	{ "rightwin",SDLK_RSUPER },
+	{ "mode",SDLK_MODE },		/* "Alt Gr" key */
+	{ "compose",SDLK_COMPOSE },	/* Multi-key compose key */
 
 	/* Miscellaneous function keys */
-	"help",SDLK_HELP,
-	"print",SDLK_PRINT,
-	"sysreq",SDLK_SYSREQ,
-	"break",SDLK_BREAK,
-	"menu",SDLK_MENU,
-	"power",SDLK_POWER,		/* Power Macintosh power key */
-	"euro",SDLK_EURO,		/* Some european keyboards */
-	"undo",SDLK_UNDO,		/* Atari keyboard has Undo */
-	NULL					// end of the keymap
+	{ "help",SDLK_HELP },
+	{ "print",SDLK_PRINT },
+	{ "sysreq",SDLK_SYSREQ },
+	{ "break",SDLK_BREAK },
+	{ "menu",SDLK_MENU },
+	{ "power",SDLK_POWER },	/* Power Macintosh power key */
+	{ "euro",SDLK_EURO },		/* Some european keyboards */
+	{ "undo",SDLK_UNDO },		/* Atari keyboard has Undo */
+	NULL				// end of the keymap
 };	
 
 std::string InputManager::keyToString (const SDLKey key)
@@ -621,6 +613,7 @@ void InputManager::currentConfigToConfigFileForRightKeyboard()
 void InputManager::configFileToCurrentConfigForLeftJoystick()
 {
 	mConfigManager.loadFile("inputconfig.xml");
+	mLeftBlobbyJoystickNumber = mConfigManager.getInteger("left_blobby_joystick_number");
 	mLeftBlobbyLeftJoystickMove = stringToInt(mConfigManager.getString("left_blobby_left_joystick_move"));
 	mLeftBlobbyRightJoystickMove = stringToInt(mConfigManager.getString("left_blobby_right_joystick_move"));
 	mLeftBlobbyJoystickJump = stringToInt(mConfigManager.getString("left_blobby_joystick_jump"));
@@ -632,9 +625,14 @@ void InputManager::configFileToCurrentConfigForLeftJoystick()
 void InputManager::configFileToCurrentConfigForRightJoystick()
 {
 	mConfigManager.loadFile("inputconfig.xml");
+	mRightBlobbyJoystickNumber = mConfigManager.getInteger("right_blobby_joystick_number");
 	mRightBlobbyLeftKeyboardMove = stringToKey(mConfigManager.getString("right_blobby_left_joystick_move"));
 	mRightBlobbyRightKeyboardMove = stringToKey(mConfigManager.getString("right_blobby_right_joystick_move"));
 	mRightBlobbyKeyboardJump = stringToKey(mConfigManager.getString("right_blobby_joystick_jump"));
+	mRightBlobbyJoystickJump = stringToInt(mConfigManager.getString("right_blobby_joystick_jump"));
+	mRightBlobbyJoystickDiagonal = stringToInt(mConfigManager.getString("right_blobby_joystick_diagonal"));
+	mRightBlobbyJoystickLeftJump = stringToInt(mConfigManager.getString("right_blobby_left_joystick_jump"));
+	mRightBlobbyJoystickRightJump = stringToInt(mConfigManager.getString("right_blobby_right_joystick_jump"));
 }
 
 void InputManager::currentConfigToConfigFileForLeftJoystick()
