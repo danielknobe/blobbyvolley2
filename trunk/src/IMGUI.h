0b64806931340a4cea6539947f13276cd0398cb8
svenrech <svenrech@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2006-09-19 18:01:14 +0000

    doInactiveMode

--- trunk/src/IMGUI.h
+++ trunk/src/IMGUI.h
@@ -30,12 +30,14 @@ public:
 	void doImage(int id, const Vector2& position, const std::string& name);
 	void doText(int id, const Vector2& position, const std::string& text);
 	void doOverlay(int id, const Vector2& pos1, const Vector2& pos2, const Color& col = Color(0, 0, 0));
-	void doCursor();
+	void doCursor(bool draw = true) { mDrawCursor = draw; }
 
 	bool doButton(int id, const Vector2& position, const std::string& text);
 	bool doScrollbar(int id, const Vector2& position, float& value);
 	bool doEditbox(int id, const Vector2& position, std::string& text, int& cpos);
 	bool doBlob(int id, const Vector2& position, const Color& col);
+
+	void doInactiveMode(bool inactive) { mInactive = inactive; }
 private:
 	IMGUI();
 	~IMGUI();
@@ -47,4 +49,5 @@ private:
 	int mLastWidget;
 	bool mDrawCursor;
 	bool mButtonReset;
+	bool mInactive;
 };
