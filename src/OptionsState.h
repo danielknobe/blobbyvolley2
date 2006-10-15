5b8ebb724fa78ddfe3d3023f48c1312ec0dd562e
svenrech <svenrech@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2006-10-15 15:11:13 +0000

    seperated the OptionMenus from the State.cpp file

---
+++ src/OptionsState.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "Global.h"
+#include "UserConfig.h"
+#include "State.h"
+
+class OptionState : public State
+{
+public:
+	OptionState();
+	virtual ~OptionState();
+	virtual void step();
+private:
+	UserConfig mOptionConfig;
+	std::vector<std::string> mScriptNames;
+	int mPlayerOptions[MAX_PLAYERS];
+	bool mSaveConfig;	
+};
+
+class GraphicOptionsState : public State
+{
+public:
+	GraphicOptionsState();
+	virtual ~GraphicOptionsState();
+	virtual void step();
+private:
+	UserConfig mOptionConfig;
+	bool mSaveConfig;
+	bool mFullscreen;
+	std::string mRenderer;
+	int mR1, mG1, mB1, mR2, mG2, mB2;
+	bool mLeftMorphing, mRightMorphing;
+};
+
+class InputOptionsState : public State
+{
+public:
+	InputOptionsState();
+	virtual ~InputOptionsState();
+	virtual void step();
+private:
+	UserConfig mOptionConfig;
+	bool mSaveConfig;
+	std::string oldString;
+	int oldInteger;
+	//left data:
+	std::string mLeftBlobbyDevice;
+	int mLeftBlobbyMouseJumpbutton;
+	std::string mLeftBlobbyKeyboardLeft;
+	std::string mLeftBlobbyKeyboardRight;
+	std::string mLeftBlobbyKeyboardJump;
+	std::string mLeftBlobbyJoystickLeft;
+	std::string mLeftBlobbyJoystickRight;
+	std::string mLeftBlobbyJoystickJump;
+	//right data:
+	std::string mRightBlobbyDevice;
+	int mRightBlobbyMouseJumpbutton;
+	std::string mRightBlobbyKeyboardLeft;
+	std::string mRightBlobbyKeyboardRight;
+	std::string mRightBlobbyKeyboardJump;
+	std::string mRightBlobbyJoystickLeft;
+	std::string mRightBlobbyJoystickRight;
+	std::string mRightBlobbyJoystickJump;
+};
+
+class MiscOptionsState : public State
+{
+public:
+	MiscOptionsState();
+	virtual ~MiscOptionsState();
+	virtual void step();
+private:
+	UserConfig mOptionConfig;
+	std::vector<std::string> mBackgrounds;
+	int mBackground;
+	float mVolume;
+	int mGameFPS;
+	bool mShowFPS;
+	bool mSaveConfig;	
+};
