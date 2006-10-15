9e26ff940f38cbe9dc58ac14e807a4053035ef45
yonibear <yonibear@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2006-10-15 15:41:16 +0000

    Documented Network Messages

--- src/NetworkMessage.h
+++ src/NetworkMessage.h
@@ -11,7 +11,15 @@ enum MessageType
 	ID_BALL_RESET,
 	ID_BALL_GROUND_COLLISION,
 	ID_BALL_PLAYER_COLLISION,
+	ID_GAME_READY,
 };
+
+// General Information:
+// 	Because the client may choose their side and the server rotates
+// 	everything if necessary, PlayerSide informations may not be
+// 	correct on all peers. When the server sends a side information
+// 	to a client, the information has to be converted into the view
+// 	of the client.
 
 // ID_INPUT_UPDATE:
 // 	Description:
@@ -20,10 +28,10 @@ enum MessageType
 // 	Structure:
 // 		ID_INPUT_UPDATE
 // 		ID_TIMESTAMP
-// 		timestamp 
-// 		left keypress
-// 		right keypress
-// 		up keypress
+// 		timestamp (int)
+// 		left keypress (bool)
+// 		right keypress (bool)
+// 		up keypress (bool)
 //
 // ID_PHYSIC_UPDATE:
 // 	Description:
@@ -33,9 +41,53 @@ enum MessageType
 // 	Structure:
 // 		ID_PHYSIC_UPDATE
 // 		ID_TIMESTAMP
-// 		timestamp
+// 		timestamp (int)
 // 		Physic data (analysed by PhysicWorld)
 //
 // ID_WIN_NOTIFICATION
 // 	Description:
-// 		Message sent from server to all clients.
+// 		Message sent from server to all clients when a player
+// 		won the game. The appended enum tells the client which
+// 		player won.
+// 	Structure:
+// 		ID_WIN_NOTIFICATION
+// 		winning player (PlayerSide)
+// 
+// ID_BALL_RESET
+// 	Description:
+// 		 Message sent from server to all clients when the ball
+// 		 is reset to the starting position. It includes an information
+// 		 about the current point state.
+// 	Structure:
+// 		ID_BALL_RESET
+// 		serving player (PlayerSide)
+// 		left points (char)
+// 		right points (char)
+//
+// ID_BALL_GROUND_COLLISION
+// 	Description:
+// 		Message sent from server to all clients when the ball
+// 		hits the ground. It is the only valid reason for a whistle
+// 		sound during the game. The side attribute tells on which side
+// 		the ball hit the ground.
+// 	Structure:
+// 		ID_BALL_GROUND_COLLISION
+//		side (PlayerSide)
+//
+// ID_BALL_PLAYER_COLLISION
+// 	Description:
+// 		Message sent from server to all clients when the ball
+// 		hits a player.  It is the only valid reason for a player
+// 		collision sound. The player attribute tells which player
+// 		hit the ball
+// 	Structure:
+// 		ID_BALL_PLAYER_COLLISION
+// 		player (PlayerSide)
+//
+// ID_GAME_READY
+// 	Description:
+// 		Message sent from server to client when all clients are
+// 		ready. The input is enabled after this message on the client.
+// 	Structure:
+// 		ID_GAME_READY
+
