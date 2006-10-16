34f235f7a74f2fbc4bbba95d966fb587e7a2a563
danielknobe <danielknobe@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2006-10-16 20:05:10 +0000

    - physik works know without any bug (i hope ... -_-)

--- trunk/src/PhysicWorld.cpp
+++ trunk/src/PhysicWorld.cpp
@@ -390,10 +390,10 @@ void PhysicWorld::step()
 			mBallPosition,Vector2(NET_POSITION_X,NET_POSITION_Y-NET_SPHERE)
 			).length() <= NET_RADIUS + BALL_RADIUS && mBallPosition.y < NET_POSITION_Y-NET_SPHERE)
 		{
+			mBallPosition -= mBallVelocity/TIMESTEP;
 			mBallVelocity = mBallVelocity.reflect(
             Vector2(mBallPosition,Vector2(NET_POSITION_X,NET_POSITION_Y-NET_SPHERE))
 			.normalise()).scale(0.75);
-			mBallPosition += mBallVelocity;
 		}
 		
 		mBlobPosition[LEFT_PLAYER] += mBlobVelocity[LEFT_PLAYER]/TIMESTEP;
