3431eb55271b8b30202f1f32eaff0ba9ef36a8b2
yonibear <yonibear@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2007-08-14 11:12:50 +0000

    Converted to cmake

--- trunk/src/OptionsState.cpp
+++ trunk/src/OptionsState.cpp
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "LocalInputSource.h"
 #include "SpeedController.h"
 #include "SoundManager.h"
-#include "blood.h"
+#include "Blood.h"
 #include "IMGUI.h"
 
 #include <physfs.h>
