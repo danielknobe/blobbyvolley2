33add0ce942f817a3a50e5c7b575af18bfd5aa41
yonibear <yonibear@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2007-08-19 13:52:06 +0000

    Restructured some server code

--- src/DedicatedServer.cpp
+++ src/DedicatedServer.cpp
@@ -42,59 +42,70 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #undef main
 #endif
 
+static bool g_run_in_foreground = false;
+static bool g_print_syslog_to_stderr = false;
+static bool g_workaround_memleaks = false;
+
 void printHelp()
 {
 	std::cout << "Usage: blobby-server [OPTION...]" << std::endl;
+	std::cout << "  -m, --memleak-hack        Workaround memory leaks by restarting regularly" << std::endl;
 	std::cout << "  -n, --no-daemon           Don´t run as background process" << std::endl;
 	std::cout << "  -p, --print-msgs          Print messages to stderr" << std::endl;
 	std::cout << "  -h, --help                This message" << std::endl;
 }
 
-int main(int argc, char** argv)
+void process_arguments(int argc, char** argv)
 {
-	bool run_in_foreground = false;
-	bool print_syslog_to_stderr = false;
 	if (argc > 1)
 	{
 		for (int i = 1; i < argc; ++i)
 		{
+			if (strcmp(argv[i], "--memleak-hack") == 0 || strcmp(argv[i], "-m") == 0)
+			{
+				g_workaround_memleaks = true;
+				continue;
+			}
 			if (strcmp(argv[i], "--no-daemon") == 0 || strcmp(argv[i], "-n") == 0)
 			{
-				run_in_foreground = true;
+				g_run_in_foreground = true;
 				continue;
 			}
 			if (strcmp(argv[i], "--print-msgs") == 0 || strcmp(argv[i], "-p") == 0)
 			{
-				print_syslog_to_stderr = true;
+				g_print_syslog_to_stderr = true;
 				continue;
 			}
 			if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0)
 			{
 				printHelp();
-				return 3;
+				exit(3);
 			}
 			std::cout << "Unknown option \"" << argv[i] << "\"" << std::endl;
 			printHelp();
-			return 1;
+			exit(1);
 		}
 	}
-	if (!run_in_foreground)
+}
+
+void fork_to_background()
+{
+	pid_t f_return = fork();
+	if (f_return == -1)
 	{
-		pid_t f_return = fork();
-		if (f_return == -1)
-		{
-			perror("fork");
-			return 1;
-		}
-		if (f_return != 0)
-		{
-			std::cout << "Running in background as PID " << f_return << std::endl;
-			return 0;
-		}
+		perror("fork");
+		exit(1);
+	}
+	if (f_return != 0)
+	{
+		std::cout << "Running in background as PID " << f_return << std::endl;
+		exit(0);
 	}
 
-	// Memory leak workaround with restart:
-	
+}
+
+void wait_and_restart_child()
+{
 	pid_t leaking_server;
 	while ((leaking_server = fork()) > 0)
 	{
@@ -102,7 +113,6 @@ int main(int argc, char** argv)
 
 		// Wait for server to quit and refork
 		waitpid(leaking_server, &status, 0);
-
 		// Error will propably occur again
 		if (WEXITSTATUS(status) != 0)
 		{
@@ -113,22 +123,44 @@ int main(int argc, char** argv)
 	if (leaking_server == -1)
 	{
 		perror("fork");
-		return 1;
+		exit(1);
 	}
 
-	int startTime = SDL_GetTicks();
+}
 
-	int syslog_options = LOG_CONS | LOG_PID;
-	if (print_syslog_to_stderr)
-		syslog_options |= LOG_PERROR;
-	openlog("blobby-server", syslog_options, LOG_DAEMON);
-	PHYSFS_init(argv[0]);
+void setup_physfs(char* argv0)
+{
+	PHYSFS_init(argv0);
 	PHYSFS_addToSearchPath("data", 1);
 	std::string userdir = PHYSFS_getUserDir();
 	std::string userAppend = ".blobby";
 	std::string homedir = userdir + userAppend;
 	PHYSFS_addToSearchPath(userdir.c_str(), 0);
 	PHYSFS_setWriteDir(userdir.c_str());
+
+}
+
+int main(int argc, char** argv)
+{
+	process_arguments(argc, argv);
+	
+	if (!g_run_in_foreground)
+	{
+		fork_to_background();
+	}
+
+	if (g_workaround_memleaks)
+	{
+		wait_and_restart_child();
+	}
+	
+
+	int startTime = SDL_GetTicks();
+
+	int syslog_options = LOG_CONS | LOG_PID | (g_print_syslog_to_stderr ? LOG_PERROR : 0);
+
+	openlog("blobby-server", syslog_options, LOG_DAEMON);
+	setup_physfs(argv[0]);
 
 	GameList gamelist;
 	PlayerMap playermap;
@@ -352,14 +384,17 @@ int main(int argc, char** argv)
 		server.DeallocatePacket(packet);
 		scontroller.update();
 
-		// Workaround for memory leak
-		// Restart the server after 1 hour if no player is
-		// connected
-		if ((SDL_GetTicks() - startTime) > 60 * 60 * 1000)
+		if (g_workaround_memleaks)
 		{
-			if (gamelist.empty() && firstPlayerSide == NO_PLAYER)
+			// Workaround for memory leak
+			// Restart the server after 1 hour if no player is
+			// connected
+			if ((SDL_GetTicks() - startTime) > 60 * 60 * 1000)
 			{
-				exit(0);
+				if (gamelist.empty() && firstPlayerSide == NO_PLAYER)
+				{
+					exit(0);
+				}
 			}
 		}
 	}
