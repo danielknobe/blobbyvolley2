ffa5d1e1b85cf00d534fa4e83c6e71df24082b30
danielknobe <54905085+danielknobe@users.noreply.github.com>
2007-09-14 14:47:15 +0000

    - bot-fix

--- data/scripts/axji.lua
+++ data/scripts/axji.lua
@@ -4,7 +4,7 @@ end
 
 wait = 0
 serv = true
-aggroservice = 64 -- versetzung zum Ball je grösser desto tiefer fliegt der Service 0-64
+aggroservice = 64 -- versetzung zum Ball je grï¿½sser desto tiefer fliegt der Service 0-64
 
 
 function OnServe(ballready)
@@ -14,7 +14,7 @@ function OnServe(ballready)
 	if ballready then -- ball bereit
 		if wait > 90 then -- 90 einheiten gewartet
 			jump() -- Springen also eigentlich den Ball aufwerfen
-			wait = 0 -- Wait timer zurücksetzen
+			wait = 0 -- Wait timer zurï¿½cksetzen
 		end
 	end
 end
@@ -24,12 +24,12 @@ function OnGame()
 
 	-- bestimmen wenn der service vertig ist
 	if (ballx() > 400) then -- sobald der ball auf der anderen seite ist ;)
-		serv = false -- serv flag zurücksetzen
+		serv = false -- serv flag zurï¿½cksetzen
 	end
 	
 	if serv then -- sprunganschlag code
-		moveto(estimate()-aggroservice) -- Bewegt sich unter den Ball und schlägt mit einer gewissen aggressivität an bei 64 knapp übers Netz
-		-- warscheinlich wäre ein Match.random() angebracht
+		moveto(estimate()-aggroservice) -- Bewegt sich unter den Ball und schlï¿½gt mit einer gewissen aggressivitï¿½t an bei 64 knapp ï¿½bers Netz
+		-- warscheinlich wï¿½re ein Match.random() angebracht
 		if bally() < 550 then -- wenn der Ball wieder runter kommt
 			jump()
 		end
@@ -41,6 +41,3 @@ function OnGame()
 		end
 	end
 end
-<div style='padding: 4px; background-color: #2B3647; color: white'>PHP-Fehler</div><div style='padding: 4px; background-color: #F5F6F9; border: 1px solid #2B3647'>
-Fatal error: Call to a member function run_hooks() on a non-object in /home/www-data/htdocs/blobby/forum/inc/functions.php on line 146
-</div><div style='padding: 4px; background-color: #2B3647; text-align: right'><a style='color: white; font-size: 0.8em' href='http://www.redio.de/'>Redio Webhosting</a></div>
