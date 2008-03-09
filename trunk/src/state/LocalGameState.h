fec5b845146af1667994e6f7b0ca00d7688f9f1e
yonibear <yonibear@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2008-03-09 15:05:32 +0000

    Forgot to add new state files

---
+++ trunk/src/state/LocalGameState.h
@@ -0,0 +1,46 @@
+/*=============================================================================
+Blobby Volley 2
+Copyright (C) 2008 Jonathan Sieber (jonathan_sieber@yahoo.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+#pragma once
+
+#include "Player.h"
+#include "State.h"
+
+class LocalGameState : public State
+{
+private:
+	Player mLeftPlayer;
+	Player mRightPlayer;
+	
+	bool mLeftOscillate;
+	bool mRightOscillate;
+	bool mPaused;
+	bool mSaveReplay;
+	bool mWinner;
+	std::string mFilename;
+	
+	DuelMatch* mMatch;
+	ReplayRecorder* mRecorder;
+public:
+	LocalGameState();
+	virtual ~LocalGameState();
+	virtual void step();
+	
+};
+
