8f91410b1b1e98a962711bb9a7a01f8dfd007a89
danielknobe <54905085+danielknobe@users.noreply.github.com>
2010-03-07 14:50:38 +0000

    - fix cmake buildfiles

--- src/NetworkGame.cpp
+++ src/NetworkGame.cpp
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "NetworkGame.h"
 #include "NetworkMessage.h"
+#include "GameLogic.h"
 #include "raknet/RakServer.h"
 #include "raknet/BitStream.h"
 #include "raknet/GetTime.h"
