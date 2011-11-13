1bd88fb3c674510b81194b6c43fbaaf2f3d43b0e
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2011-11-13 18:11:58 +0000

    further reduced include dependencies:  Global.h no longer includes whole SDL library

--- trunk/src/InputDevice.h
+++ trunk/src/InputDevice.h
@@ -20,10 +20,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #pragma once
 
 #include <map>
+#include <SDL/SDL_events.h>
 #include "Global.h"
 #include "InputSource.h"
-
-//class SDL_Joystick;
 
 class JoystickPool
 {
