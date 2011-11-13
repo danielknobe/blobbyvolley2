b76f62a6babc6b26e19c090185751987faff17d0
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2011-11-13 17:45:20 +0000

    moved InputDevice code from header into cpp file reduced some include dependencies

--- trunk/src/state/NetworkState.cpp
+++ trunk/src/state/NetworkState.cpp
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <algorithm>
 #include <sstream>
+#include <iostream>
 #include <ctime>
 
 #include "NetworkState.h"
