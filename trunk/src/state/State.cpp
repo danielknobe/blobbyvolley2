28a1774b872c7a865533a04ec246560b16e3779a
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2011-11-21 20:09:37 +0000

    refactored State.h/State.cpp: ReplayState moved in own file

--- trunk/src/state/State.cpp
+++ trunk/src/state/State.cpp
@@ -18,22 +18,21 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 =============================================================================*/
 
 #include "State.h"
-#include "state/LocalGameState.h"
-#include "LocalInputSource.h"
-#include "DuelMatch.h"
-#include "ReplayRecorder.h"
-#include "ScriptedInputSource.h"
-#include "SoundManager.h"
-#include "IMGUI.h"
+
+#include "LocalGameState.h"
+#include "ReplayState.h"
 #include "NetworkState.h"
 #include "OptionsState.h"
+
+#include "DuelMatch.h"
+#include "SoundManager.h"
+#include "IMGUI.h"
 #include "TextManager.h"
 #include "SpeedController.h"
 #include "Blood.h"
 
 #include <physfs.h>
 #include <algorithm>
-#include <sstream>
 
 State* State::mCurrentState = 0;
 
@@ -218,158 +217,4 @@ void CreditsState::step()
 		return;
 	}
 }
-
-ReplayMenuState::ReplayMenuState() :
-	mLeftPlayer(LEFT_PLAYER),
-	mRightPlayer(RIGHT_PLAYER)
-{
-	IMGUI::getSingleton().resetSelection();
-	mReplaying = false;
-	mChecksumError = false;
-
-	mReplayMatch = 0;
-	mReplayRecorder = 0;
-	mSelectedReplay = 0;
-	char** filenames = PHYSFS_enumerateFiles("replays");
-	for (int i = 0; filenames[i] != 0; ++i)
-	{
-		std::string tmp(filenames[i]);
-		if (tmp.find(".bvr") != std::string::npos)
-		{
-			mReplayFiles.push_back(std::string(tmp.begin(), tmp.end()-4));
-		}
-	}
-	if (mReplayFiles.size() == 0)
-		mSelectedReplay = -1;
-	std::sort(mReplayFiles.rbegin(), mReplayFiles.rend());
-
-	mLeftPlayer.loadFromConfig("left");
-	mRightPlayer.loadFromConfig("right");
-}
-
-void ReplayMenuState::loadCurrentReplay()
-{
-	mReplayRecorder = new ReplayRecorder(MODE_REPLAY_DUEL);
-
-	try
-	{
-		mReplayRecorder->load(std::string("replays/" + mReplayFiles[mSelectedReplay] + ".bvr"));
-		mReplaying = true;
-		mReplayMatch = new DuelMatch(0, 0, true, false);
-		mReplayMatch->setServingPlayer(mReplayRecorder->getServingPlayer());
-		RenderManager::getSingleton().setPlayernames(
-			mReplayRecorder->getPlayerName(LEFT_PLAYER), mReplayRecorder->getPlayerName(RIGHT_PLAYER));
-		SoundManager::getSingleton().playSound(
-				"sounds/pfiff.wav", ROUND_START_SOUND_VOLUME);
-	}
-	catch (ChecksumException& e)
-	{
-		delete mReplayRecorder;
-		mReplayRecorder = 0;
-		mChecksumError = true;
-	}
-}
-
-void ReplayMenuState::step()
-{
-	IMGUI& imgui = IMGUI::getSingleton();
-	if (mReplaying)
-	{
-		RenderManager* rmanager = &RenderManager::getSingleton();
-
-		if(mReplayRecorder->getPacketType()==ID_INPUT)
-		{
-			mReplayMatch->setPlayersInput(mReplayRecorder->getInput());
-			mReplayMatch->step();
-		}
-		
-		presentGame(*mReplayMatch);
-		rmanager->setBlobColor(LEFT_PLAYER, mLeftPlayer.getColor());
-		rmanager->setBlobColor(RIGHT_PLAYER, mRightPlayer.getColor());
-
-		PlayerSide side = mReplayMatch->winningPlayer();
-		if (side != NO_PLAYER)
-		{
-			std::stringstream tmp;
-			if(side == LEFT_PLAYER)
-				tmp << mReplayRecorder->getPlayerName(LEFT_PLAYER);
-			else
-				tmp << mReplayRecorder->getPlayerName(RIGHT_PLAYER);
-			imgui.doOverlay(GEN_ID, Vector2(200, 150), Vector2(650, 450));
-			imgui.doImage(GEN_ID, Vector2(200, 250), "gfx/pokal.bmp");
-			imgui.doText(GEN_ID, Vector2(274, 250), tmp.str());
-			imgui.doText(GEN_ID, Vector2(274, 300), TextManager::getSingleton()->getString(TextManager::GAME_WIN));
-			if (imgui.doButton(GEN_ID, Vector2(290, 350), TextManager::getSingleton()->getString(TextManager::LBL_OK)))
-			{
-				mReplaying = false;
-				delete mReplayMatch;
-				delete mReplayRecorder;
-				imgui.resetSelection();
-			}
-			if (imgui.doButton(GEN_ID, Vector2(400, 350), TextManager::getSingleton()->getString(TextManager::RP_SHOW_AGAIN)))
-			{
-				delete mReplayMatch;
-				delete mReplayRecorder;
-				loadCurrentReplay();
-				imgui.resetSelection();
-			}
-			imgui.doCursor();
-		}
-		else if ((InputManager::getSingleton()->exit()) || (mReplayRecorder->endOfFile()))
-		{
-			mReplaying = false;
-			delete mReplayMatch;
-			delete mReplayRecorder;
-			imgui.resetSelection();
-		}
-	}
-	else
-	{
-		imgui.doCursor();
-		imgui.doImage(GEN_ID, Vector2(400.0, 300.0), "background");
-		imgui.doOverlay(GEN_ID, Vector2(0.0, 0.0), Vector2(800.0, 600.0));
-
-		if (imgui.doButton(GEN_ID, Vector2(224.0, 10.0), TextManager::getSingleton()->getString(TextManager::RP_PLAY)) &&
-					mSelectedReplay != -1)
-		{
-			loadCurrentReplay();
-			imgui.resetSelection();
-		}
-		else if (imgui.doButton(GEN_ID, Vector2(424.0, 10.0), TextManager::getSingleton()->getString(TextManager::LBL_CANCEL)))
-		{
-			deleteCurrentState();
-			setCurrentState(new MainMenuState());
-		}
-		else
-			imgui.doSelectbox(GEN_ID, Vector2(34.0, 50.0), Vector2(634.0, 550.0), mReplayFiles, mSelectedReplay);
-		if (imgui.doButton(GEN_ID, Vector2(644.0, 60.0), TextManager::getSingleton()->getString(TextManager::RP_DELETE)))
-		{
-			if (!mReplayFiles.empty())
-			if (PHYSFS_delete(std::string("replays/" + mReplayFiles[mSelectedReplay] + ".bvr").c_str()))
-			{
-				mReplayFiles.erase(mReplayFiles.begin()+mSelectedReplay);
-				if (mSelectedReplay >= mReplayFiles.size())
-					mSelectedReplay = mReplayFiles.size()-1;
-			}
-		}
-
-		if (mChecksumError)
-		{
-			imgui.doInactiveMode(false);
-			imgui.doOverlay(GEN_ID, Vector2(210, 180), Vector2(650, 370));
-			imgui.doText(GEN_ID, Vector2(250, 200), TextManager::getSingleton()->getString(TextManager::RP_CHECKSUM));
-			imgui.doText(GEN_ID, Vector2(250, 250), TextManager::getSingleton()->getString(TextManager::RP_FILE_CORRUPT));
-
-			if (imgui.doButton(GEN_ID, Vector2(400, 330), TextManager::getSingleton()->getString(TextManager::LBL_OK)))
-			{
-				mChecksumError = false;
-			}
-			else
-			{
-				imgui.doInactiveMode(true);
-			}
-		}
-	}
-}
-
 
