ac88adc99b387caa5f326bf0c98e3b4ba91bfe36
ngc92 <7938269+ngc92@users.noreply.github.com>
2011-12-01 13:14:13 +0000

    using boost::accumulators in test

--- test/LagDetectionTest.cpp
+++ test/LagDetectionTest.cpp
@@ -11,6 +11,14 @@
 #include <cmath>
 #include <iostream>
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/count.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/variates/covariate.hpp>
+using namespace boost::accumulators;
+
 PlayerInput randomInputGen()
 {
 	return PlayerInput( rand() % 2, rand() % 2, rand() % 2);
@@ -67,10 +75,9 @@ struct LaggingNetworkInputSimulator
 };
 
 /// Fixture for creating a LagDetector
-template<int SetupLag>
 struct LagDetectionSetup
 {
-	LagDetectionSetup() : D (LagDetector()), NetworkSimulator(LaggingNetworkInputSimulator( SetupLag ))
+	LagDetectionSetup() : D (LagDetector()), NetworkSimulator(LaggingNetworkInputSimulator( 0 ))
 	{
 	}
 	
@@ -139,7 +146,7 @@ BOOST_AUTO_TEST_CASE( self_correlation )
 
 
 // test that checks wether LagDetector finds zero lag when no lag is present
-BOOST_FIXTURE_TEST_CASE( zero_lag, LagDetectionSetup<0> )
+BOOST_FIXTURE_TEST_CASE( zero_lag, LagDetectionSetup )
 {
 	// insert zero lag data
 	simulate(10);
@@ -161,7 +168,7 @@ BOOST_FIXTURE_TEST_CASE( zero_lag, LagDetectionSetup<0> )
 
 // test that checks wether LagDetector finds constant lags
 /// \todo use http://www.boost.org/doc/libs/1_48_0/libs/test/doc/html/utf/user-guide/test-organization/unary-test-case.html
-BOOST_FIXTURE_TEST_CASE( constant_lag, LagDetectionSetup<1> )
+BOOST_FIXTURE_TEST_CASE( constant_lag, LagDetectionSetup )
 {
 	file << "\nconstant lag" << "\n";
 	// test for all small constant lags
@@ -192,7 +199,7 @@ const int REAL_INPUT_PATTERN_LENGTH = 40;
 const int INPUT_CHANGE_STEP_WIDTH = 5;
 
 // check constant lag with bad input data
-BOOST_FIXTURE_TEST_CASE( constant_lag_real_input, LagDetectionSetup<0> )
+BOOST_FIXTURE_TEST_CASE( constant_lag_real_input, LagDetectionSetup )
 {
 	file << "\nconstant lag - real input" << "\n";
 	result << "constant lag - real input\n";
@@ -242,26 +249,27 @@ const int LAG_CHANGE_RATE = 10;
 // in this test, we can see how fast die algorithm can detect changing lag
 // in this test, the lag changes are only slight, so more than 1frame per change
 // does not happen
-BOOST_FIXTURE_TEST_CASE( changing_lag, LagDetectionSetup<0> )
+BOOST_FIXTURE_TEST_CASE( changing_lag, LagDetectionSetup )
 {
-	file << "\nchanging lag - real input" << "\n";
+	file << "\nchanging lag - good input" << "\n";
 	result << "changing lag - good input - small change\n";
 	
 	// test for all small constant lags
 	int clag = rand() % 8 + 4;
 	NetworkSimulator.changePing(clag);
 	
+	// start a game
 	simulate(100);
 	
-	// now we do 500 steps and check for lag
-	/// \todo maybe we should use boost.accumulator
+	// accumulator for collecting statistical data of our run
+	accumulator_set< int, features< tag::count, tag::max, tag::mean, tag::mean_of_variates<int, tag::covariate1> > > acc;
+	
 	int lag = 0;
 	int errors = 0;
 	int timer = 0;
-	int changes = 0;
 	int cum_timer = 0;
-	int cum_diff_timer = 0;
-	int max_timer = 0;
+
+	// now we do 500 steps and check for lag
 	for(int i = 0; i < 500; ++i)
 	{		
 		// randomly change lags. we are friendly for now.
@@ -274,7 +282,6 @@ BOOST_FIXTURE_TEST_CASE( changing_lag, LagDetectionSetup<0> )
 			clag = nclag;
 			NetworkSimulator.changePing(clag);
 			timer = 0;
-			changes++;
 		}
 		simulate();
 		
@@ -289,9 +296,9 @@ BOOST_FIXTURE_TEST_CASE( changing_lag, LagDetectionSetup<0> )
 			file << lag << " " << timer << "\n" << D.getDebugString() << "\n";
 			
 			// calculate cum timer
+			
 			cum_timer += timer;
-			max_timer = std::max(max_timer, std::max(0, timer - lag));
-			cum_diff_timer += std::max(0, timer - lag);
+			acc(std::max(0, timer - lag), covariate1 = timer);
 			timer = 0;
 		}
 	}
@@ -299,16 +306,16 @@ BOOST_FIXTURE_TEST_CASE( changing_lag, LagDetectionSetup<0> )
 	// when we take longer than 10ms to detect the lag change after the first packet with new lag arrived
 	// we are too slow. 
 	// when we take longer than 15ms once, it is bad, too
-	if( cum_diff_timer / changes > 5  || max_timer > 15)
+	if( mean(acc) > 5  || max(acc) > 15)
 	{
 		char errormsg[1024];
-		sprintf(errormsg, "LagDetector takes too long to detect lag change of 1ms. Add: %d, Avg: %d, Max: %d",  cum_diff_timer / changes, cum_timer/changes, max_timer);
+		sprintf(errormsg, "LagDetector takes too long to detect lag change of 1ms. Add: %d, Avg: %d, Max: %d",  (int)mean(acc), (int)boost::accumulators::mean_of_variates<int, tag::covariate1>(acc), max(acc));
 		BOOST_ERROR(errormsg);
 	}
 	
-	result << "maximum reaction time: " << max_timer << std::endl;
-	result << "average reaction time: " << cum_timer / changes << std::endl;
-	result << "average additional reaction time: " << cum_diff_timer / changes << std::endl;
+	result << "maximum reaction time: " << max(acc) << std::endl;
+	result << "average reaction time: " << boost::accumulators::mean_of_variates<int, tag::covariate1>(acc) << std::endl;
+	result << "average additional reaction time: " << mean(acc) << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
