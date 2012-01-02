8eb9818febb6f58c6e93d657484e4d8d5b42fd05
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2012-01-02 15:20:20 +0000

    removed physfs includes

--- trunk/src/state/State.cpp
+++ trunk/src/state/State.cpp
@@ -32,7 +32,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "SpeedController.h"
 #include "Blood.h"
 
-#include <physfs.h>
 #include <algorithm>
 
 State* State::mCurrentState = 0;
