1e64a15d91557f4f1c59d43248859b7f4b54e532
ngc92 <7938269+ngc92@users.noreply.github.com>
2012-01-04 21:42:10 +0000

    initialise NetworkPlayer to 0.0.0.0:0

--- src/main.cpp
+++ src/main.cpp
@@ -204,7 +204,7 @@ int main(int argc, char* argv[])
 			// disable it if necessary
 			rmanager->drawGame(true);
 			IMGUI::getSingleton().begin();
-				State::getCurrentState()->step();
+			State::getCurrentState()->step();
 			rmanager = &RenderManager::getSingleton(); //RenderManager may change
 			//draw FPS:
 			if (scontroller.getDrawFPS())
