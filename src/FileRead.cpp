a4de505886ba25831a7286c9f756f563757a59b2
danielknobe <54905085+danielknobe@users.noreply.github.com>
2012-01-16 09:06:50 +0000

    - some cmake fixes - include Global.h and not global.h. On case-sensitive systems this will result in an error

--- src/FileRead.cpp
+++ src/FileRead.cpp
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "FileRead.h"
 
 #include <physfs.h>
-#include "global.h"
+#include "Global.h"
 
 #include <cassert>
 
