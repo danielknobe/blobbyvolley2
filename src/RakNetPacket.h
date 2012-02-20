98c27d1f3b05b02a157729abec21d369dab54510
ngc92 <7938269+ngc92@users.noreply.github.com>
2012-02-20 13:14:10 +0000

    code style improvements

--- src/RakNetPacket.h
+++ src/RakNetPacket.h
@@ -26,20 +26,26 @@ class Packet;
 
 typedef boost::shared_ptr<Packet> packet_ptr;
 
-struct deleter{
+struct deleter
+{
 	RakPeer* peer;
-	void operator()(Packet* p){
+	void operator()(Packet* p)
+	{
 		peer->DeallocatePacket(p);
 	}
 };
 
-inline packet_ptr receivePacket(RakPeer* peer){
+inline packet_ptr receivePacket(RakPeer* peer)
+{
 	deleter del;
 	del.peer = peer;
 	Packet* pptr = peer->Receive();
-	if(pptr){
+	if(pptr)
+	{
 		return packet_ptr(pptr, del);
-	}else{
+	} 
+	else 
+	{
 		return packet_ptr();
 	}	
 }
