8a551e724225750758bb3cd8455d61b60eb9be8d
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2012-03-29 20:31:58 +0000

    player.cpp:  scoped_ptr memory management  errors during input source creation no longer crash the game

--- trunk/src/Player.cpp
+++ trunk/src/Player.cpp
@@ -22,6 +22,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Player.h"
 
 /* includes */
+#include <iostream> // for cerr
+
 #include "LocalInputSource.h"
 #include "ScriptedInputSource.h"
 #include "UserConfig.h"
@@ -35,9 +37,6 @@ Player::Player(PlayerSide side) :
 
 Player::~Player()
 {
-	if (mInitialised) {
-		delete mInputSource;
-	}
 }
 
 void Player::loadFromConfig(const std::string& prefix, bool initInput)
@@ -48,14 +47,24 @@ void Player::loadFromConfig(const std::string& prefix, bool initInput)
 	// init local input
 	if(initInput)
 	{
-		if (gameConfig.getBool(prefix + "_player_human")) 
+		try
 		{
-			mInputSource = new LocalInputSource(mPlayerSide);
-		} 
-		else 
+			// these operations may throw, i.e., when the script is not found (should not happen)
+			//  or has errors
+			if (gameConfig.getBool(prefix + "_player_human")) 
+			{
+				mInputSource.reset(new LocalInputSource(mPlayerSide));
+			} 
+			else 
+			{
+				mInputSource.reset(new ScriptedInputSource("scripts/" +
+					gameConfig.getString(prefix + "_script_name"), mPlayerSide, gameConfig.getInteger(prefix + "_script_strength")));
+			}
+		} catch (std::exception& e)
 		{
-			mInputSource = new ScriptedInputSource("scripts/" +
-				gameConfig.getString(prefix + "_script_name"), mPlayerSide, gameConfig.getInteger(prefix + "_script_strength"));
+			/// \todo REWORK ERROR REPORTING
+			std::cerr << e.what() << std::endl;
+			mInputSource.reset(new DummyInputSource());
 		}
 	
 		mName = gameConfig.getBool(prefix + "_player_human") ?
@@ -66,7 +75,7 @@ void Player::loadFromConfig(const std::string& prefix, bool initInput)
 	{
 		// input is set externally (network)
 		// so we need not to create any input source
-		mInputSource = 0;
+		mInputSource.reset();
 		// don't use bot name if extern input is used
 		mName = gameConfig.getString(prefix + "_player_name");
 	}
@@ -86,9 +95,12 @@ Color Player::getColor() const
 {
 	assert(mInitialised);
 
-	if (!mOscillating) {
+	if (!mOscillating) 
+	{
 		return mStaticColor;
-	} else {
+	}
+	 else 
+	{
 		float time = float(SDL_GetTicks()) / 1000.0;
 		return Color(
 			int((sin(time*2) + 1.0) * 128),
@@ -119,6 +131,6 @@ InputSource* Player::getInputSource() const
 	assert(mInitialised);
 	assert(mInputSource != 0);
 
-	return mInputSource;
+	return mInputSource.get();
 }
 
