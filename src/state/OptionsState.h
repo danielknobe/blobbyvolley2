e0ae6931c9bc9bccfaeb37dcad4db260ee1cde66
ngc92 <7938269+ngc92@users.noreply.github.com>
2012-03-29 20:33:30 +0000

    fixed missing HUMAN in player selection

--- src/state/OptionsState.h
+++ src/state/OptionsState.h
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "Global.h"
 #include "UserConfig.h"
 #include "State.h"
+#include <vector>
 
 /*! \class OptionState
 	\brief State for managing the main options menu
