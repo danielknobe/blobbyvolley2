cf1a0e29f9b26e10b125d5a96beb1aaa158ec7b7
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2012-10-20 13:26:32 +0000

    updated version and changelog

--- trunk/src/Global.h
+++ trunk/src/Global.h
@@ -37,9 +37,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 const int BLOBBY_PORT = 1234;
 
 const int BLOBBY_VERSION_MAJOR = 0;
-const int BLOBBY_VERSION_MINOR = 100;
+const int BLOBBY_VERSION_MINOR = 101;
 
-const char AppTitle[] = "Blobby Volley 2 Version 1.0 RC2";
+const char AppTitle[] = "Blobby Volley 2 Version 1.0 RC3";
 
 const float ROUND_START_SOUND_VOLUME = 0.2;
 const float BALL_HIT_PLAYER_SOUND_VOLUME = 0.4;
