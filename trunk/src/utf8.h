03d9e5542356e5482ec5574aa7ebb7d16923c6b6
danielknobe <danielknobe@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2012-12-31 00:42:45 +0000

    extended utf8 implementation

--- trunk/src/utf8.h
+++ trunk/src/utf8.h
@@ -1,7 +1,28 @@
+/*=============================================================================
+Blobby Volley 2
+Copyright (C) 2006 Jonathan Sieber (jonathan_sieber@yahoo.de)
+Copyright (C) 2006 Daniel Knobe (daniel-knobe@web.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
 
 #pragma once
 
-// utility functions for handling utf8
+/// @brief move wchar_t string to utf8 character sequence
+/// @param codepoint String of data
+/// @param target Target char array which will contain utf8 sequence
 inline void to_utf8(wchar_t codepoint, char* target)
 {
 	if(codepoint <= 0x7F)
@@ -13,29 +34,49 @@ inline void to_utf8(wchar_t codepoint, char* target)
 		unsigned char high = codepoint >> 8;
 		unsigned char low = codepoint & 0xFF;
 		// codepoint: 00000xxx|xxyyyyyy
-		// => 		  110xxxxx|10yyyyyy
+		// =>         110xxxxx|10yyyyyy
 		target[0] = 0xC0 | (0x1F & (high << 2 | low >> 6 ) );
 		target[1] = 0x80 | (0x3F & low);
 	} else 
 	{
+		//TODO: Missing implementation
 	}
 }
 
-// get following char length
+/// @brief get utf8 character char length  
+/// @param first First char of utf8 character
 inline int getCodepointLength(char first)
 {
-	// 1 byte = 
-	if( first >> 7 == 0)
+	// if 0xxx xxxx
+	if (!(first & 0x80))
 	{
 		return 1;
-	} 
-	
-	if( first >> 5 == 5)
+	}
+
+	// if x0xx xxxx
+	if (!(first & 0x40))
+	{
+		return 1;
+	}
+
+	// if xx0x xxxx
+	if (!(first & 0x20))
 	{
 		return 2;
 	}
-	// \todo implement more cases
+
+	// if xxx0 xxxx
+	if (!(first & 0x10))
+	{
+		return 3;
+	}
+
+	// if xxxx 0xxx
+	if (!(first & 0x08))
+	{
+		return 4;
+	}
 	
-	// no starting point
-	return 0;
+	// for other cases, threat as 1 character
+	return 1;
 }
