d1c039451cbcbf289200f3180c091a9cc09aef59
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2013-10-21 18:37:43 +0000

    fixes

--- trunk/src/state/NetworkSearchState.cpp
+++ trunk/src/state/NetworkSearchState.cpp
@@ -41,7 +41,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "NetworkState.h"
 #include "TextManager.h"
 #include "IMGUI.h"
-#include "RakNetPacket.h"
 #include "IUserConfigReader.h"
 #include "FileWrite.h"
 #include "FileRead.h"
