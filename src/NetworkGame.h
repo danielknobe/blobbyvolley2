0313833ed0dacc2b9c463393b8f9a3b4ea545cfc
ngc92 <7938269+ngc92@users.noreply.github.com>
2013-10-21 17:03:50 +0000

    server refactoring: separated console program from server logic

--- src/NetworkGame.h
+++ src/NetworkGame.h
@@ -29,7 +29,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "raknet/BitStream.h"
 #include "SpeedController.h"
 #include "InputSource.h"
-#include "RakNetPacket.h"
 #include "DuelMatch.h"
 #include "BlobbyDebug.h"
 
