8c1d47b198a6f8ed727f583210b72f3295782a96
danielknobe <54905085+danielknobe@users.noreply.github.com>
2013-10-31 14:23:57 +0000

    - filedescription in codeconventions, will be changed soon

--- src/Vector.h
+++ src/Vector.h
@@ -18,6 +18,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 =============================================================================*/
 
+/**
+ * @file Vector.h
+ * @brief Contains a class to handle two dimensinal vectors
+ */
+
 #pragma once
 
 #include <cassert>
