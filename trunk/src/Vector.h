ba3dae4757e344fce75892b86ccba8ea9453e04e
danielknobe <danielknobe@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2013-10-31 14:24:56 +0000

    fix typo

--- trunk/src/Vector.h
+++ trunk/src/Vector.h
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /**
  * @file Vector.h
- * @brief Contains a class to handle two dimensinal vectors
+ * @brief Contains a class to handle two dimensional vectors
  */
 
 #pragma once
