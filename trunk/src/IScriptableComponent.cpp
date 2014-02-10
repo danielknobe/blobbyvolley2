94f2e6e0a81e802321442e5597bc8a58fd064c79
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2014-02-10 16:16:34 +0000

    unified set game constants

--- trunk/src/IScriptableComponent.cpp
+++ trunk/src/IScriptableComponent.cpp
@@ -1,5 +1,9 @@
 #include "IScriptableComponent.h"
+
 #include "lua/lua.hpp"
+
+#include "Global.h"
+#include "GameConstants.h"
 
 IScriptableComponent::IScriptableComponent() : mState(luaL_newstate())
 {
@@ -27,4 +31,23 @@ bool IScriptableComponent::getLuaFunction(const char* fname) const
 	}
 
 	return true;
+}
+
+void IScriptableComponent::setGameConstants()
+{
+	// set game constants
+	setLuaGlobal("CONST_FIELD_WIDTH", RIGHT_PLANE);
+	setLuaGlobal("CONST_GROUND_HEIGHT", GROUND_PLANE_HEIGHT_MAX);
+	setLuaGlobal("CONST_BALL_GRAVITY", -BALL_GRAVITATION);
+	setLuaGlobal("CONST_BALL_RADIUS", BALL_RADIUS);
+	setLuaGlobal("CONST_BLOBBY_JUMP", BLOBBY_JUMP_ACCELERATION);
+	setLuaGlobal("CONST_BLOBBY_BODY_RADIUS", BLOBBY_LOWER_RADIUS);
+	setLuaGlobal("CONST_BLOBBY_HEAD_RADIUS", BLOBBY_UPPER_RADIUS);
+	setLuaGlobal("CONST_BLOBBY_HEIGHT", BLOBBY_HEIGHT);
+	setLuaGlobal("CONST_BLOBBY_GRAVITY", GRAVITATION);
+	setLuaGlobal("CONST_NET_HEIGHT", NET_SPHERE_POSITION);
+	setLuaGlobal("CONST_NET_RADIUS", NET_RADIUS);
+	setLuaGlobal("NO_PLAYER", NO_PLAYER);
+	setLuaGlobal("LEFT_PLAYER", LEFT_PLAYER);
+	setLuaGlobal("RIGHT_PLAYER", RIGHT_PLAYER);
 }
