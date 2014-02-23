d04b90713c430473a8ab71650d545d9f6a589178
ngc92 <7938269+ngc92@users.noreply.github.com>
2014-02-23 20:40:07 +0000

    player input cleanup

--- src/InputSource.cpp
+++ src/InputSource.cpp
@@ -22,116 +22,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "InputSource.h"
 
 /* includes */
-#include <ostream>
 #include <cassert>
 
-#include "raknet/BitStream.h"
-
-#include "DuelMatch.h"
-#include "GameConstants.h"
-
 /* implementation */
-
-/* PlayerInputAbs */
-
-PlayerInputAbs::PlayerInputAbs() : mFlags( F_RELATIVE ), mTarget(-1)
-{
-
-}
-
-PlayerInputAbs::PlayerInputAbs(RakNet::BitStream& stream)
-{
-	stream.Read( mFlags );
-	stream.Read( mTarget );
-}
-
-PlayerInputAbs::PlayerInputAbs(bool l, bool r, bool j) : mFlags( F_RELATIVE ), mTarget(-1)
-{
-	setLeft(l);
-	setRight(r);
-	setJump(j);
-}
-
-
-// set input
-void PlayerInputAbs::setLeft( bool v )
-{
-	if(v)
-		mFlags |= F_LEFT;
-	else
-		mFlags &= ~F_LEFT;
-}
-
-void PlayerInputAbs::setRight( bool v )
-{
-	if(v)
-		mFlags |= F_RIGHT;
-	else
-		mFlags &= ~F_RIGHT;
-}
-
-void PlayerInputAbs::setJump( bool v)
-{
-	if(v)
-		mFlags |= F_JUMP;
-	else
-		mFlags &= ~F_JUMP;
-}
-
-void PlayerInputAbs::setTarget( short target, PlayerSide player )
-{
-	mFlags &= F_JUMP;	// reset everything but the jump flag, i.e. no left/right and no relative
-	mTarget = target;
-
-	if(player == LEFT_PLAYER )
-	{
-		setLeft(true);
-	}
-	if(player == RIGHT_PLAYER )
-	{
-		setRight(true);
-	}
-}
-
-void PlayerInputAbs::swapSides()
-{
-	bool left = mFlags & F_LEFT;
-	bool right = mFlags & F_RIGHT;
-
-	setLeft(right);
-	setRight(left);
-
-	mTarget = RIGHT_PLANE - mTarget;
-}
-
-PlayerInput PlayerInputAbs::toPlayerInput( const DuelMatch* match ) const
-{
-	if( mFlags & F_RELATIVE)
-		return PlayerInput( mFlags & F_LEFT, mFlags & F_RIGHT, mFlags & F_JUMP );
-	else
-	{
-		bool left = false;
-		bool right = false;
-
-		PlayerSide side = mFlags & F_LEFT ? LEFT_PLAYER : RIGHT_PLAYER;
-
-		// here we load the current position of the player.
-		float blobpos = match->getBlobPosition(side).x;
-
-		if (blobpos + BLOBBY_SPEED * 2 <= mTarget)
-			right = true;
-		else if (blobpos - BLOBBY_SPEED * 2 >= mTarget)
-			left = true;
-		return PlayerInput( left, right, mFlags & F_JUMP );
-	}
-
-}
-
-void PlayerInputAbs::writeTo(RakNet::BitStream& stream)
-{
-	stream.Write( mFlags );
-	stream.Write( mTarget );
-}
 
 /* InputSource */
 
@@ -186,8 +79,3 @@ void InputSource::setMatch(const DuelMatch* match)
 	mMatch = match;
 }
 
-std::ostream& operator<< (std::ostream& out, const PlayerInput& input)
-{
-	out << (input.left ? 't' : 'f') << (input.right ? 't' : 'f') << (input.up ? 't' : 'f');
-	return out;
-}
