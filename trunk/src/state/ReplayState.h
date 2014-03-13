a9ac23113f75b2fd13ebc08c8442cea570896a02
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2014-03-13 12:45:24 +0000

    moved GameState into separate file

--- trunk/src/state/ReplayState.h
+++ trunk/src/state/ReplayState.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <boost/scoped_ptr.hpp>
 
-#include "State.h"
+#include "GameState.h"
 #include "Vector.h"
 
 class DuelMatch;
