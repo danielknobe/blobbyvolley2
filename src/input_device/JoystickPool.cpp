81040266170ea0ed35e73775dfdd4d8328a84ec5
danielknobe <54905085+danielknobe@users.noreply.github.com>
2014-07-06 16:32:56 +0000

    - very simple joystick gui handling. Should be improved!

--- src/input_device/JoystickPool.cpp
+++ src/input_device/JoystickPool.cpp
@@ -124,13 +124,71 @@ std::string JoystickAction::toString()
 {
 	const char* typestr = "unknown";
 	if (type == AXIS)
+	{
 		typestr = "axis";
+	}
 
 	if (type == BUTTON)
+	{
 		typestr = "button";
+	}
 
 	std::stringstream buf;
 	buf << "joy_" << joyid << "_" << typestr << "_" << number;
 	return buf.str();
+}
+
+KeyAction JoystickAction::toKeyAction()
+{
+	// This is an place holder
+	// Just to get simple gamepad support for the GUI
+	// TODO: Fix that! Use GUID -> ButtonMapping to get this think cleaned up
+	if (type == AXIS)
+	{
+		// X-Achse?!
+		if (abs(number) == 1)
+		{
+
+			if (number < 0)
+			{
+				return KeyAction::LEFT;
+			}
+			else
+			{
+				return KeyAction::RIGHT;
+			}
+		}
+
+		// Y-Achse?!
+		if (abs(number) == 2)
+		{
+
+			if (number < 0)
+			{
+				return KeyAction::UP;
+			}
+			else
+			{
+				return KeyAction::DOWN;
+			}
+		}
+	}
+	
+	if (type == BUTTON)
+	{
+		if(number == 0)
+		{
+			return KeyAction::SELECT;
+		}
+		if(number == 1)
+		{
+			// TODO: This is stupid, we must check if this button is used ingame so that
+			// the jumpbutton does not pause the game
+			//return JoystickEvent::BACK;
+			// We don't allow back at the moment -> game is not leavable
+			return KeyAction::NONE;
+		}
+	}
+	return KeyAction::NONE;
 }
 
