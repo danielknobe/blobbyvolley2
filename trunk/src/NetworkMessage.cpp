30900189fdf4fbef6c7ab62fbc792557cb99bbd5
ngc92 <ngc92@5dffb9df-0d16-0410-88a9-e08e82185bbb>
2015-02-25 12:00:33 +0000

    added inlude

--- trunk/src/NetworkMessage.cpp
+++ trunk/src/NetworkMessage.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /* includes */
 #include <cstring>
+#include <ostream>
 
 #include "UserConfig.h"
 #include "SpeedController.h"
