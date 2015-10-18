bca288226ed00e628a010d249734fb7e49234e4e
ngc92 <7938269+ngc92@users.noreply.github.com>
2015-10-18 19:54:10 +0000

    small fixes thanks@adamo314

--- src/server/MatchMaker.h
+++ src/server/MatchMaker.h
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "raknet/NetworkTypes.h"
 #include <map>
 #include <functional>
-#include "global.h"
+#include "Global.h"
 
 class NetworkPlayer;
 class NetworkGame;
