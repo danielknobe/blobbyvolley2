b538547d4dc2ba3af3a4f0df934766608fa5ccc4
danielknobe <daniel-knobe@web.de>
2020-12-20 23:14:48 +0100

    fix memleak... delete client after disconnect

--- src/state/LobbyStates.cpp
+++ src/state/LobbyStates.cpp
@@ -1,5 +1,27 @@
+/*=============================================================================
+Blobby Volley 2
+Copyright (C) 2006 Jonathan Sieber (jonathan_sieber@yahoo.de)
+Copyright (C) 2006 Daniel Knobe (daniel-knobe@web.de)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+/* header include */
 #include "LobbyStates.h"
 
+/* includes */
 #include <stdexcept>
 #include <algorithm>
 #include <iostream>
@@ -20,7 +42,7 @@ LobbySubstate::~LobbySubstate()
 
 
 LobbyState::LobbyState(ServerInfo info, PreviousState previous) :
-		mClient(new RakClient(), [](RakClient* client) { client->Disconnect(25); }),
+		mClient(new RakClient(), [](RakClient* client) { client->Disconnect(25); delete client; }),
 		mInfo(info), mPrevious( previous ),
 		mLobbyState(ConnectionState::CONNECTING)
 {
