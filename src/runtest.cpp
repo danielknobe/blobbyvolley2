6db361a5f3d2e3b356e1d4ef734c8d05e080510e
ngc92 <7938269+ngc92@users.noreply.github.com>
2022-09-24 18:25:27 +0300

    update runtest to be compatible with latest changes

--- src/runtest.cpp
+++ src/runtest.cpp
@@ -248,7 +248,7 @@ int main(int argc, char* argv[])
 			if (!scontroller.doFramedrop())
 			{
 				rmanager->draw();
-				IMGUI::getSingleton().end();
+				IMGUI::getSingleton().end(*rmanager);
 				rmanager->getBlood().step(*rmanager);
 				rmanager->refresh();
 			}
